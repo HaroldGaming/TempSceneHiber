ef5baa5f734e3602658abfe429d5699fbbb83717
NeoPower <catplayer007@hotmail.com>
2017-03-01 16:45:22 +0100

    ..
    ..

---
+++ TempRumble/Assets/Scripts/FightControll.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FightControll : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        if (Input.touchCount == 2) {
+            print("Attack");
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            print("bleh");
+        }
+    }
+}
