66ca5d3f83219964febfed3556be036d42fa48e1
HaroldGaming <harold-de-jong@hotmail.com>
2017-03-02 17:11:22 +0100

    .
    .

---
+++ TempRumble/Assets/Scripts/AI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+
+    private Dash dashClass;
+    public float dashTime;
+
+	void Start () {
+        dashClass = GetComponent<Dash>();
+        StartCoroutine(Dashing(0));
+	}
+
+    IEnumerator Dashing(int leftRight) {
+        dashClass.Dasher(leftRight);
+        yield return new WaitForSeconds(dashTime);
+        leftRight = Random.Range(0, 2);
+        StartCoroutine(Dashing(leftRight));
+    }
+}
--- TempRumble/Assets/Scripts/Dash.cs
+++ TempRumble/Assets/Scripts/Dash.cs
@@ -2,7 +2,62 @@
 using System.Collections;
 
 public class Dash : MonoBehaviour {
-        
+
+    public float dashTime, dashSpeed;
+    private float time;
+    private bool dashingRight, dashingLeft;
+    private Transform target;
+
+    void Awake() {
+        if(transform.tag == "Player1") {
+            target = GameObject.FindGameObjectWithTag("Player2").transform;
+        }
+        else {
+            target = GameObject.FindGameObjectWithTag("Player1").transform;
+        }
+    }
+
+    void Update() {
+        if (dashingRight) {
+            transform.RotateAround(target.position, Vector3.up, +dashSpeed * Time.deltaTime);
+        }
+        else {
+
+        }
+
+        if (dashingLeft){
+            transform.RotateAround(target.position, Vector3.up, -dashSpeed * Time.deltaTime);
+        }
+
+        if(time >= 0) {
+            time -= Time.deltaTime;
+
+        }
+        else {
+            dashingRight = false;
+            dashingLeft = false;
+        }
+
+    }
+
+    public void Dasher(int leftRight) {
+        time = dashTime;
+        switch (leftRight) {
+            case 0:
+                print("dash left");
+                //transform.RotateAround(GameObject.FindGameObjectWithTag("Player2").transform.position, Vector3.up, +dashRange);
+                dashingLeft = true;
+                break;
+            case 1:
+                //transform.RotateAround(GameObject.FindGameObjectWithTag("Player2").transform.position, Vector3.up, -dashRange);
+                dashingRight = true;
+                break;
+        }
+    }
+
+    IEnumerator Dashing() {
+        yield return new WaitForSeconds(3);
+    }
 
 }
 
