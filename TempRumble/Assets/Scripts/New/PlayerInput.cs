2cab052fd3ba751d9e226adf7fbe939b445a27db
HaroldGaming <harold-de-jong@hotmail.com>
2017-03-16 16:08:47 +0100

    .
    .

---
+++ TempRumble/Assets/Scripts/New/PlayerInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+    private PlayerAttack playerAttackClass;
+    private PlayerDefense playerDefenseClass;
+    private PlayerDash playerDashClass;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
